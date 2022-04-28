9c7664e1b0605454c1b9fc844b7c06ee5f9fd0f4
alanguo <alan_y_guo@manulife.com>
2022-04-28 14:54:42 +0800

    init

--- vue-back.sh
+++ vue-back.sh
@@ -7,9 +7,9 @@ git clean -f
 echo '拉取新代码'
 git pull origin master
 echo '开始执行构建'
-docker build -t vue-back .
+docker build -t vue-back:1.0 .
 echo '停止旧容器并删除旧容器'
 docker stop vue-back-container
 docker rm vue-back-container
 echo '启动新容器'
-docker container run -p 3000:3000 --name vue-back-container -d vue-back
+docker container run -p 3000:3000 --name vue-back-container -d vue-back:1.0
