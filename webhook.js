637dd13cc7d8ab171737236c5ccf99a8d4df2eea
alanguo <alan_y_guo@manulife.com>
2022-04-28 16:42:49 +0800

    init

--- webhook.js
+++ webhook.js
@@ -18,7 +18,7 @@ let server = http.createServer(function (req, res) {
       buffers.push(buffer);
     });
     req.on("end", function (buffer) {
-      let body = Buffer.contact(buffers);
+      let body = Buffer.concat(buffers);
       let event = req.headers["x-github-event"]; //event=push
       //github 请求来的时候，要传递请求体body,另外还会传一个signature过来，你需要验证签名
       let signature = req.headers["x-hub-signature"];
